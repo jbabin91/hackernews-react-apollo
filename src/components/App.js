cf22af139e07229795962b20aac63bac97905bf3
Jace Babin <jbabin91@gmail.com>
2018-07-01 22:06:15 -0700

    Finished Authentication Chapter

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,23 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Login from './Login';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="center w85">
-        <Header />
-        <div className="ph3 pv1 background-gray">
-          <Switch>
-            <Route exact path="/" component={LinkList} />
-            <Route exact path="/create" component={CreateLink} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="center w85">
+				<Header />
+				<div className="ph3 pv1 background-gray">
+					<Switch>
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/create" component={CreateLink} />
+						<Route exact path="/" component={LinkList} />
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
