66f2d24052d695545930436636b3dbc2016c4e78
Jace Babin <jbabin91@gmail.com>
2018-07-06 17:04:43 -0700

    Started working on Realtime Updates with GraphQL Subscriptions

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,24 @@ import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import Login from './Login';
+import Search from './Search';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="center w85">
-				<Header />
-				<div className="ph3 pv1 background-gray">
-					<Switch>
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/create" component={CreateLink} />
-						<Route exact path="/" component={LinkList} />
-					</Switch>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/search" component={Search} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
